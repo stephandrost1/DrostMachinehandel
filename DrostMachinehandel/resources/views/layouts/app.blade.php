1b77e92e69816c4ea1d14673f51477d1864363c6
Stephan <StephanDrost4@gmail.com>
2022-07-25 12:38:52 +0200

    Clean Tailwind installation

--- DrostMachinehandel/resources/views/layouts/app.blade.php
+++ DrostMachinehandel/resources/views/layouts/app.blade.php
@@ -1,14 +1,35 @@
 <html>
     <head>
+        {{-- Page title tag --}}
         <title>Drost Machinehandel - @yield('title')</title>
+
+        {{-- Scripts --}}
+
+        {{-- Tailwind CSS --}}
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script>
+            tailwind.config = {
+              theme: {
+                extend: {
+                  colors: {
+                    primary: '#E56D01',
+                    secondary: '#707070',
+                  }
+                }
+              }
+            }
+          </script>
+        
     </head>
     <body>
+        {{-- Navbar --}}
         @include('layouts.navbar')
  
         <div class="container">
             @yield('content')
         </div>
 
+        {{-- Footer --}}
         @include('layouts.footer')
     </body>
 </html>
