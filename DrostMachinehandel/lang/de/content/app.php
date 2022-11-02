c4ce5048a495e5031208c0e563b7f52cfd590a09
Stephan <StephanDrost4@gmail.com>
2022-11-02 13:05:27 +0100

    wip

--- DrostMachinehandel/lang/de/content/app.php
+++ DrostMachinehandel/lang/de/content/app.php
@@ -5,8 +5,10 @@
 return [
     'Home' => 'Home',
     'Voorraad' => 'Lager',
+    'Verhuur' => 'Vermietung',
     'Leasen' => 'Mieten',
     'Contact' => 'Kontakt',
+    'Login' => 'Anmeldung',
     'Dashboard' => 'Armaturenbrett',
     'Dashboard-payments' => 'Zahlungen',
     'Dashboard-tasks' => 'Aufgaben',
