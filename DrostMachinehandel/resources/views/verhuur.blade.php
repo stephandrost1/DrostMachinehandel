6a8181ca33dbe0e07f339ea401dc901636253b40
Stephan <StephanDrost4@gmail.com>
2022-11-04 15:59:00 +0100

    wip

--- DrostMachinehandel/resources/views/verhuur.blade.php
+++ DrostMachinehandel/resources/views/verhuur.blade.php
@@ -5,12 +5,37 @@
 
 <div class="verhuur-wrapper px-5 md:px-20 py-7">
     <div class="verhuur-content">
-            <div class="filter-wrapper" style="flex-basis: 25%">
-                <div class="result-amount result-amount-mobile">{{ count($machines) }} Resultaten gevonden</div>
-                <div class="filter-button">
+            <div class="total-filter-wrapper">
+                <div id="result_amount_mobile" class="result-amount result-amount-mobile">{{ count($machines) }} Resultaten gevonden</div>
+                <div id="hide_filters" class="close-filters-button">
+                    <span class="hide-filter-text">Filters</span>
+                    <span style="float: right;" onclick="hideFilters()"><i class="fas fa-times"></i></span>
+                </div>
+                <div id="filter_button" onclick="showFilters()" class="filter-button">
                     Filter
                 </div>
-                <div class="filters">
+                <div id="active_filters" class="active-filters">
+                    <div>
+                        <div class="active-filter">
+                            <span class="delete-filter-button">
+                                <i class="fas fa-times"></i>
+                            </span>
+                            <span class="delete-filter-name">
+                                Machine (4)
+                            </span>
+                        </div>
+                        <div class="active-filter">
+                            <span class="delete-filter-button">
+                                <i class="fas fa-times"></i>
+                            </span>
+                            <span class="delete-filter-name">
+                                BT (4)
+                            </span>
+                        </div>
+                    </div>
+                    <div class="delete-active-filters">Verwijder alle filters</div>
+                </div>
+                <div id="filters" class="filters">
                     <div class="filter-wrapper">
                         <span class="filter-name">Categorie
                             <i class="fas fa-chevron-up filter-arrow"></i>
@@ -30,7 +55,7 @@
                     </div>
                 </div>
             </div>
-            <div class="results-content">
+            <div id="results_content" class="results-content">
                 <div class="result-amount result-amount-desktop">{{ count($machines) }} Resultaten gevonden</div>
                 <div class="machines-wrapper">
                     @foreach ($machines as $machine)
@@ -70,6 +95,44 @@
             </div>
     </div>
 </div>
+
+<script>
+    function showFilters() {
+      var filters = document.getElementById("filters");
+      var hide_filters = document.getElementById("hide_filters");
+      var active_filters = document.getElementById("active_filters");
+      var filter_button = document.getElementById("filter_button");
+      var result_amount_mobile = document.getElementById("result_amount_mobile");
+      var results_content = document.getElementById("results_content");
+
+        filters.style.display = "block";
+        hide_filters.style.display = "block";
+        active_filters.style.display = "block";
+
+        results_content.style.display = "none";
+        filter_button.style.display = "none";
+        result_amount_mobile.style.display = "none";
+    }
+
+    function hideFilters() {
+        var filters = document.getElementById("filters");
+        var hide_filters = document.getElementById("hide_filters");
+        var active_filters = document.getElementById("active_filters");
+        var filter_button = document.getElementById("filter_button");
+        var result_amount_mobile = document.getElementById("result_amount_mobile");
+        var results_content = document.getElementById("results_content");
+
+
+        hide_filters.style.display = "none";
+        filters.style.display = "none";
+        active_filters.style.display = "none";
+
+        filter_button.style.display = "block";
+        results_content.style.display = "block";
+        result_amount_mobile.style.display = "block";
+        
+    }
+</script>
 
 
 @endsection
