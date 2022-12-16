5e27d68c2ce91a01498bb5b2dcceed1d4c4fa829
Stephan <StephanDrost4@gmail.com>
2022-12-16 11:31:26 +0100

    Input

--- DrostMachinehandel/resources/vue/components/verhuur/vehicle/delivery/delivery.vue
+++ DrostMachinehandel/resources/vue/components/verhuur/vehicle/delivery/delivery.vue
@@ -16,7 +16,7 @@ export default {
 <template>
     <div class="row flex justify-between gap-5">
         <div
-            class="input-label w-1/4 h-12 bg-white border-2 border-primary px-4 py-1 flex items-center justify-center text-primary rounded-lg">
+            class="input-label w-5/12 h-12 bg-white border-2 border-primary px-4 py-1 flex items-center justify-center text-primary rounded-lg">
             <span class="w-full">Levertijd</span>
         </div>
         <input placeholder="Prijs per week" v-model="content" @change="_handleInput" type="number"
