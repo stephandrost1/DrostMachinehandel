7ab0eb7522008e94192267e62b3c097560afc4d3
hhannewijk <git-bot@inventivetalent.org>
2022-12-16 12:28:13 +0100

    wip

--- DrostMachinehandel/resources/vue/components/verhuur/vehicle/stock/stockBlock.vue
+++ DrostMachinehandel/resources/vue/components/verhuur/vehicle/stock/stockBlock.vue
@@ -30,7 +30,7 @@ export default {
 
 <template>
         <div class="row flex justify-between gap-5">
-            <div class="input-label w-1/4 h-12 bg-white border-2 border-primary px-4 py-1 flex items-center justify-center text-primary rounded-lg">
+            <div class="input-label w-5/12 h-12 bg-white border-2 border-primary px-4 py-1 flex items-center justify-center text-primary rounded-lg">
                 <span class="w-full">Aantal beschikbaar</span>
             </div>
             <input placeholder="Bijv.. 10" v-model="content" @change="_handleInput" type="number" class="w-1/2 h-12 rounded-lg border-2 border-primary pl-2" />
