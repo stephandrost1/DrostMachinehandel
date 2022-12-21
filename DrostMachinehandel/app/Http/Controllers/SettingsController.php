b240c5c2002900c5210a8074e828a5dffb31be3e
hhannewijk <git-bot@inventivetalent.org>
2022-12-21 23:51:04 +0100

    wip

--- DrostMachinehandel/app/Http/Controllers/SettingsController.php
+++ DrostMachinehandel/app/Http/Controllers/SettingsController.php
@@ -3,14 +3,47 @@
 namespace App\Http\Controllers;
 
 use App\Models\Setting;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class SettingsController extends Controller
 {
     public static function fetchSetting($sQuery)
     {
-        $setting = Setting::where("name", $sQuery)->get()->first();
+        try {
+            $setting = Setting::where("name", $sQuery)->get()->first();
 
-        return $setting->value;
+            if (empty($setting)) {
+                Log::error("Unknow setting :" . $sQuery);
+                return null;
+            }
+
+            return $setting->value;
+        } catch (Exception $e) {
+            Log::emergency("fetchSetting", [
+                "error" => $e->getMessage()
+            ]);
+        }
+    }
+
+    public static function setSetting($sQuery, $sValue)
+    {
+        try {
+            $setting = Setting::updateOrCreate([
+                [
+                    'name' => $sQuery,
+                    'value' => $sValue,
+                ],
+                [
+                    'name' => $sQuery,
+                    'value' => $sValue,
+                ]
+            ]);
+        } catch (Exception $e) {
+            Log::emergency("setSetting", [
+                "error" => $e->getMessage()
+            ]);
+        }
     }
 }
