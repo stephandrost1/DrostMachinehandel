33c540a3a1bd3cb4cb7f98ac79f2f8f61722811e
Stephan <StephanDrost4@gmail.com>
2023-01-03 17:38:57 +0100

    Add responsive dealer-create styling

--- DrostMachinehandel/resources/views/dashboard/dealerCreate.blade.php
+++ DrostMachinehandel/resources/views/dashboard/dealerCreate.blade.php
@@ -7,18 +7,14 @@
     <form action="{{ route("dealer-create-request") }}" method="POST">
         @csrf
         
-        <div class="flex flex-col max-w-[700px] md:flex-row md:gap-20 md:justify-center bg-white rounded-xl mt-12 px-5 py-20 md:px-10 md:py-14">
-            {{-- <div class="hidden md:flex items-center">
-                <img src="{{ asset('img/login-image.png') }}" alt="IMG">
-            </div> --}}
-
+        <div class="flex flex-col max-w-[700px] md:flex-row md:gap-20 md:justify-center bg-white rounded-xl mt-8 mb-5 mx-8 px-5 py-20 md:px-10 md:py-14">
             <div class="flex items-center flex-col relative">
                 <span class="font-bold text-[#333333] text-3xl text-center w-full mb-10 md:mb-10">
                     Dealer account aanmaken
                 </span>
 
                 <div class="flex flex-col gap-3">
-                    <div class="flex items-center gap-3">
+                    <div class="flex flex-col sm:flex-row gap-3">
                         <div class="relative w-full z-10">
                             <input class="placeholder:text-[#999999] focus:ring-primary border-none font-bold text-[#666666] block w-full bg-[#e6e6e6] h-12 rounded-2xl px-10 py-5 relative" type="text" name="firstname" placeholder="Voornaam">
                             <span class="flex items-center absolute rounded-xl bottom-0 left-0 h-full pl-5">
@@ -34,7 +30,7 @@
                         </div>
                     </div>
     
-                    <div class="flex items-center gap-3">
+                    <div class="flex flex-col sm:flex-row gap-3">
                         <div class="relative w-full z-10">
                             <input class="placeholder:text-[#999999] focus:ring-primary border-none font-bold text-[#666666] block w-full bg-[#e6e6e6] h-12 rounded-2xl px-10 py-5 relative" type="text" name="email" placeholder="E-mailadres">
                             <span class="flex items-center absolute rounded-xl bottom-0 left-0 h-full pl-5">
@@ -50,7 +46,7 @@
                         </div>
                     </div>
     
-                    <div class="flex items-center gap-3">
+                    <div class="flex flex-col sm:flex-row gap-3">
                         <div class="relative w-full z-10">
                             <input class="placeholder:text-[#999999] focus:ring-primary border-none font-bold text-[#666666] block w-full bg-[#e6e6e6] h-12 rounded-2xl px-10 py-5 relative" type="text" name="country" placeholder="Land">
                             <span class="flex items-center absolute rounded-xl bottom-0 left-0 h-full pl-5">
@@ -65,7 +61,7 @@
                             </span>
                         </div>
                     </div>
-                    <div class="flex items-center gap-3">
+                    <div class="flex flex-col sm:flex-row gap-3">
                         <div class="relative w-full z-10">
                             <input class="placeholder:text-[#999999] focus:ring-primary border-none font-bold text-[#666666] block w-full bg-[#e6e6e6] h-12 rounded-2xl px-10 py-5 relative" type="text" name="city" placeholder="Plaats">
                             <span class="flex items-center absolute rounded-xl bottom-0 left-0 h-full pl-5">
@@ -80,7 +76,7 @@
                             </span>
                         </div>
                     </div>
-                    <div class="flex items-center gap-3">
+                    <div class="flex flex-col sm:flex-row gap-3">
                         <div class="relative w-full z-10">
                             <input class="placeholder:text-[#999999] focus:ring-primary border-none font-bold text-[#666666] block w-full bg-[#e6e6e6] h-12 rounded-2xl px-10 py-5 relative" type="text" name="streetname" placeholder="Straatnaam">
                             <span class="flex items-center absolute rounded-xl bottom-0 left-0 h-full pl-5">
@@ -95,7 +91,7 @@
                             </span>
                         </div>
                     </div>
-                    <div class="flex items-center gap-3">
+                    <div class="flex flex-col sm:flex-row gap-3">
                         <div class="relative w-full z-10">
                             <input class="placeholder:text-[#999999] focus:ring-primary border-none font-bold text-[#666666] block w-full bg-[#e6e6e6] h-12 rounded-2xl px-10 py-5 relative" type="text" name="company" placeholder="Bedrijfsnaam">
                             <span class="flex items-center absolute rounded-xl bottom-0 left-0 h-full pl-5">
@@ -116,7 +112,7 @@
                             </span>
                         </div>
                     </div>
-                    <div class="flex items-center gap-3">
+                    <div class="flex flex-col sm:flex-row items-center gap-3">
                         <div class="relative w-full z-10">
                             <input class="placeholder:text-[#999999] focus:ring-primary border-none font-bold text-[#666666] block w-full bg-[#e6e6e6] h-12 rounded-2xl px-10 py-5 relative" type="password" name="password" placeholder="Wachtwoord">
                             <span class="flex items-center absolute rounded-xl bottom-0 left-0 h-full pl-5">
@@ -134,7 +130,7 @@
                 </div>
 
                 @if (Session::has('errors'))
-                    <div class="text-red-500 rounded">
+                    <div class="text-red-500 rounded mt-3">
                         <ul>
                             @foreach (Session::get('errors')->all() as $error)
                                 <li>{{ $error }}</li>
@@ -143,7 +139,7 @@
                     </div>
                 @endif
                 
-                <div class="w-1/2 flex justify-center mt-1 md:mt-5">
+                <div class="w-full sm:w-1/2 flex justify-center mt-2 md:mt-5">
                     <button type="submit" class="w-full h-5 rounded-2xl bg-primary flex justify-center items-center px-0 py-6 font-bold">
                         Account aanmaken
                     </button>
